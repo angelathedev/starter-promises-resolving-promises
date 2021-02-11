45756ce2e139e58fdaa1c25c51b6a3dfad6c6dc9
Angela McByrd <76753196+angelathedev@users.noreply.github.com>
2021-02-10 23:31:21 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 
-const promise = welcome();
+const question = "Will the weather be nice today?";
+const promise = tell(question);
+
+promise.then(console.log);
 
 console.log(promise);
+
